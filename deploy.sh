43e8e505842a6dbdf41d197e1fb4be9429ddb1c6
Jeff Bucklew <bucklew.jeff@gmail.com>
2021-08-20 21:21:09 -0400

    update gh pages deploy script

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:beechovsky/ffb-draft-magic-vue.git main:gh-pages
+git push -f git@github.com:beechovsky/ffb-draft-magic-vue.git master:gh-pages
 
 cd -
 #
